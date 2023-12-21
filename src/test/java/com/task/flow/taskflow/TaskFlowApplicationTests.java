d27ec025976a751d6d4f92623e7a8b25f2a7b749
khalid fifel <fifle.khalid99@gmail.com>
2023-12-21 16:15:08 +0100

    first commit

---
+++ src/test/java/com/task/flow/taskflow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.flow.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
