bf7dfcc2a9fc8d524bda8950d10e20e03aa06a11
khalid fifel <fifle.khalid99@gmail.com>
2024-01-07 14:23:50 +0100

    fix the cors error

--- src/main/java/com/taskflow/config/AppConfig.java
+++ src/main/java/com/taskflow/config/AppConfig.java
@@ -3,6 +3,9 @@ package com.taskflow.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfig {
@@ -13,5 +16,15 @@ public class AppConfig {
         modelMapper.getConfiguration().setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
         return modelMapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
